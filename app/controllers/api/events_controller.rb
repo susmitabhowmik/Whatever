a3922d32f498acdd251965b18b8ee180834c6a54
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-04-03 23:19:26 -0400

    create events index and add Ticketmaster API to show events near latitude and longitude

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,26 @@
+class Api::EventsController < ApplicationController
+  def index
+    address = params[:address]
+    modified_address = ""
+    i = 0
+    address.length.times do 
+      if address[i] == " "
+        modified_address += "+"
+      else
+        modified_address += address[i]
+      end
+      i += 1
+    end
+
+    latlong = HTTP.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{modified_address}&key=#{ENV["GOOGLE_API_KEY"]}")
+
+    lat = latlong.parse["results"][0]["geometry"]["location"]["lat"]
+    long = latlong.parse["results"][0]["geometry"]["location"]["lng"]
+
+    radius = params[:radius]
+
+    response = HTTP.get("https://app.ticketmaster.com/discovery/v2/events.json?latlong=#{lat},#{long}&radius=#{radius}&units=km&apikey=#{ENV["TICKETMASTER_API_KEY"]}")
+
+    render json: response.parse
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   namespace :api do
     get '/restaurants' => 'restaurants#index'
 
-    post '/users' => 'users#create'
+    get '/events' => 'events#index'
 
+    post '/users' => 'users#create'
   end
 end

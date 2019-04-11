4a7e1c8b41a08b6d1fe44339ba26dc863d39352c
Susmita Bhowmik <susmita.bhowmik516@gmail.com>
2019-04-10 21:00:10 -0400

    update events controller to include pages parameter to increase variablility in the output of results

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -17,9 +17,14 @@ class Api::EventsController < ApplicationController
     lat = latlong.parse["results"][0]["geometry"]["location"]["lat"]
     long = latlong.parse["results"][0]["geometry"]["location"]["lng"]
 
+    city = params[:city]
+    state = params[:state]
+    start_date = params[:start_date] #2019-04-10T00:00:00Z
+    end_date = params[:end_date]
+    keyword= params[:keyword]
     radius = params[:radius]
 
-    response = HTTP.get("https://app.ticketmaster.com/discovery/v2/events.json?latlong=#{lat},#{long}&radius=#{radius}&units=km&apikey=#{ENV["TICKETMASTER_API_KEY"]}")
+    response = HTTP.get("https://app.ticketmaster.com/discovery/v2/events.json?&page=#{rand(1...10)}&latlong=#{lat},#{long}&radius=#{radius}&units=miles&apikey=#{ENV["TICKETMASTER_API_KEY"]}")
 
     @event = response.parse.to_a[0][1]["events"].sample
     render 'index.json.jbuilder'
--- app/views/api/events/index.json.jbuilder
+++ app/views/api/events/index.json.jbuilder
@@ -1,6 +1,6 @@
 json.name @event["name"]
 json.dateTime @event["dates"]["start"]["dateTime"]
 json.venue @event["_embedded"]["venues"][0]["name"]
-json.priceRange @event["priceRanges"][0]
+# json.priceRange @event["priceRanges"][0]
 
 # json.event @event
